cd5860cb047a7e4cd09f0c591db14814e522baf6
FabianPertuz <fpertuz117.f@gmail.com>
2025-04-03 07:35:36 -0500

    feat: :sparkles: created files: contens. json and main.py and did some of the code for the menu on main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import json
+def show_menu():
+    main_menu="""
+*************welcome***********
+1.register service
+2.edit service
+3.delete service
+4.quit
+"""
+print(main_menu)
+def ask_option ():
+    return input("enter the desired option")
+def create_service():
+
+
